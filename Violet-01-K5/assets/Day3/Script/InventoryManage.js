da51f4ad3d0385a6e9636099bc7ef7248d435f2a
Fresher-Cocos-4 <>
2025-05-08 18:01:01 +0700

    + adding script

---
+++ Violet-01-K5/assets/Day3/Script/InventoryManage.js
@@ -0,0 +1,16 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        
+
+        nameLb: cc.Label,
+        quantilityLb: cc.Label,
+        typeLb: cc.Label,
+        effectLb: cc.Label,
+        useBtn: cc.Button,
+        dropBtn: cc.Button
+    },
+
+});
